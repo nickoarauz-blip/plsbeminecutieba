314be39fb9f797498d67cf1f0f7a841e3dbc3f69
nickoarauz-blip <nickoarauz@gmail.com>
2026-02-12 04:01:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Nicole, my baba,",
+    valentineName: "Nicole, my baba",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
